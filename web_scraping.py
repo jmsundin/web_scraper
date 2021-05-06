0db3fc2c1083d3fd226044722d4ed54c5984d629
jonsundin <sundinjon@gmail.com>
2021-05-06 16:31:55 -0400

    added functions

--- web_scraping.py
+++ web_scraping.py
@@ -1,19 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def url_request(url):
     response = requests.get(url)
     try:
         response.raise_for_status()
     except Exception as ex:
         print(f'There was a problem: {ex}')
-    
-    parse_html(response)
 
-    ### writing response to a file ###
-    # with open('composing_programs.html', 'wb') as f:
-    #     for chunk in response.iter_content(1000):
-    #         f.write(chunk)
+    return response
+
+
+def write_to_file(text):
+    "Writing text to a file"
+    with open('composing_programs.txt', 'wb') as f: # TODO: create a filename by parsing the URL given by user in stdin 
+        for page in text:
+            f.write(page)
 
 
 def get_links(soup):
@@ -25,10 +28,26 @@ def get_links(soup):
     return urls
 
 
+def get_all_text(soup):
+    text = soup.get_text()
+    return text
+
+
 def parse_html(response):
     soup = BeautifulSoup(response, 'html.parser')
     urls = get_links(soup)
-    
+    text = []
+
+    # Loop through internal site links to create soup objects for each request response
+    soups = []
+    for url in urls:
+        soups.append(BeautifulSoup(url_request(url), 'html.parser'))
+
+    # Loop through list of soup objects and get all text from them
+    for soup in soups:
+        text.append(get_all_text(soup))
+
+    write_to_file(text)
 
 
 if __name__ == '__main__':
@@ -36,4 +55,9 @@ if __name__ == '__main__':
     if input():
         url = input()
     
-    url_request(url)
+    response = url_request(url)
+
+    # uncomment line below if you want the response to be written to a file
+    #write_to_file(response)
+
+    parse_html(response)
